679080eed7c7cb439be7e2275a36f090060426c0
mehadihasan916 <mehadi.womenindigital@gmail.com>
2024-11-24 10:37:51 +0600

    gallery api create

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\FoundRequest;
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
     public function index($id)
     {
+        $data_image = FoundRequest::select('image')->where('user_id', $id)->get();
+        $images = [];
 
-        return response()->json($id);
+        foreach ($data_image as $item) {
+            $decodedImages = json_decode($item->image, true);
+            if (is_array($decodedImages)) {
+                $images = array_merge($images, $decodedImages);
+            }
+        }
+
+        return response()->json(['images' => $images]);
     }
 }
